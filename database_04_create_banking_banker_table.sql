a8d809c0b4253b31000703f9f84e1164269b4d9d
Anne Yu <yu.ann.eml@gmail.com>
2025-07-12 14:11:38 +0800

    Update database_04_create_banking_banker_table.sql

--- database_04_create_banking_banker_table.sql
+++ database_04_create_banking_banker_table.sql
@@ -1,8 +1,6 @@
 USE transactions;
 
-DROP TABLE IF EXISTS banking_banker;
-
-CREATE TABLE banking_banker (
+CREATE OR REPLACE TABLE banking_banker (
     employment_id VARCHAR(20) NOT NULL PRIMARY KEY,
     employee_name VARCHAR(100) NOT NULL,
     title VARCHAR(50) NOT NULL,
@@ -29,3 +27,16 @@ VALUES (
 
 SELECT * FROM banking_banker;
 
+ALTER TABLE banking_accounts
+ADD COLUMN banker_id VARCHAR(20),
+ADD CONSTRAINT fk_banker
+FOREIGN KEY (banker_id)
+REFERENCES banking_banker(employment_id)
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+UPDATE banking_accounts
+SET banker_id = 'ABC202021'
+WHERE account_number = '101';
+
+SELECT * FROM banking_banker;
