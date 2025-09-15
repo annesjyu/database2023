26b29f619fe9c97368c4b80c045a4ae992aece44
Anne Yu <yu.ann.eml@gmail.com>
2025-09-15 15:56:50 +0800

    Add files via upload

---
+++ p2.sql
@@ -0,0 +1,93 @@
+C:\Program Files\MariaDB 12.0\bin>mysql --user=root --password=root < C:\Users\admin\Downloads\mysql_dump.sql
+
+loan, trans, account,
+ 
+constraints:
+-- Step 1: Drop the foreign key (must know the exact name)
+ALTER TABLE loan 
+DROP FOREIGN KEY fk_loan_account;
+
+-- Step 2: Add the new constraint
+ALTER TABLE loan
+ADD CONSTRAINT fk_loan_account
+FOREIGN KEY (account_id) REFERENCES account(account_id);
+
+ALTER TABLE loan
+DROP PRIMARY KEY,
+ADD CONSTRAINT PRIMARY KEY (account_id);
+
+SHOW CREATE TABLE trans;
+
+ALTER TABLE trans
+ADD CONSTRAINT PRIMARY KEY (trans_id, account_id);
+
+ALTER TABLE trans
+ADD CONSTRAINT fk_trans_account
+FOREIGN KEY (account_id) REFERENCES account(account_id);
+
+-- data types
+SELECT `date` FROM trans LIMIT 10;
+
+SELECT STR_TO_DATE(trans.date, '%y%m%d') as formatted_date from trans LIMIT 10;
+
+ALTER TABLE trans ADD column formatted_date DATE;
+
+UPDATE trans SET trans.formatted_date = STR_TO_DATE(trans.date, '%y%m%d');
+
+SELECT `date` AS original_date, formatted_date FROM trans LIMIT 10;
+
+-- missing values
+SELECT COUNT(distinct trans_id, account_id) FROM trans WHERE amount IS NULL OR amount = 0;
+
+UPDATE trans
+SET amount =
+  (SELECT AVG(amount)
+   FROM trans WHERE amount IS NOT NULL
+  OR amount <> 0)
+WHERE amount IS NULL OR amount = 0;
+
+-- duplicates
+
+SELECT trans_id, account_id, COUNT(*) FROM trans GROUP BY 1 HAVING COUNT(*) > 2;
+
+SELECT account_id, COUNT(*) FROM account GROUP BY account_id HAVING COUNT(*) > 2;
+
+-- transform
+
+SELECT DISTINCT `type` FROM trans;
+
+ALTER TABLE trans
+ADD COLUMN formatted_type VARCHAR(100);
+
+UPDATE trans
+SET formatted_type = (
+case `type`
+when 'PRIJEM' then 	'incoming credit'
+when 'VYDAJ' then 	'outgoing debit'
+when 'VYBER' then 	'withdrawal'
+ELSE NULL
+END);
+
+SELECT DISTINCT `type`, formatted_type FROM trans;
+
+-- create new joined view or table
+
+CREATE VIEW account_loan_jt AS
+  (SELECT account.account_id,
+          account.district_id,
+          account.frequency,
+          account.date AS account_date,
+          loan.loan_id,
+          loan.date AS loan_date,
+          loan.amount,
+          loan.duration,
+          loan.payments,
+          loan.`status`
+   FROM ACCOUNT
+   INNER JOIN loan ON account.account_id = loan.account_id);
+
+
+
+
+
+
